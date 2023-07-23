f2edc1964dfc12b815bb77eb52b1081690460adc
Arjun Thapa Magar <arjun2057@hotmail.com>
2023-07-23 20:27:37 +1000

    php arithmetic

--- index.php
+++ index.php
@@ -1,39 +1,44 @@
 <?php
-    $name = "Kenny";
-    $food = "Pizza";
-    $email = "fake@gmail.com";
+    // Arithmetic operators
+    // + - * / ** %
 
-    $age = 23;
-    $users = 3;
-    $quantity = 4;
+    $x = 10;
+    $y = 3;
+    $z = null;
 
-    $gpa = 2.9;
-    $price = 4.99;
-    $tax_rate = 5.1;
+    //$z = $x + $y;
+    //$z = $x - $y;
+    //$z = $x * $y;
+    //$z = $x / $y;
+    //$z = $x ** $y;
+    //$z = $x % $y;
 
-    $employed = true;
-    $online = false;
-    $for_sale = true;
+    echo $z;
+
+    //Increment/Decrement operators
+
+    $counter = 0;
+
+    //$counter = $counter + 1;
+    //$counter++;
+    //$counter+=2;
+
+    //$counter = $counter - 1;
+    //$counter--;
+    //$counter-=2;
+
+    echo $counter;
+
+    //Operator Precedence
+    // ()
+    // **
+    // * / %
+    // + -
+
+    $total = 1 + 2 - 3 * 4 / 5 ** 6;
+    echo $total;
 
 
-    echo "Hello {$name}<br>";
-    echo "You like {$food}<br>";
-    echo "Your email address is {$email}<br>";
-
-    echo "You are {$age} years old<br>";
-    echo "There are {$users} users online<br>";
-    echo "You would like to buy {$quantity} items<br>";
-
-    echo "Your gpa is: {$gpa}<br>";
-    echo "Your pizza is \${$price}<br>";
-    echo "The sales tax rate is: {$tax_rate}%<br>";
-
-    echo "Online status: {$online}<br>";
-    echo "Employment status: {$employed}<br>";
-
-    echo "You have ordered {$quantity} x {$food}s <br>";
-    $total = $quantity * $price;
-    echo "Your total is: \${$total}<br>";
 
 ?>
 
