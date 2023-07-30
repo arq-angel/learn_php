42dca7f911ca1db584a85e017bf4f373eec49301
Arjun Thapa Magar <arjun2057@hotmail.com>
2023-07-30 21:38:54 +1000

    php _SESSION

--- index.php
+++ index.php
@@ -1,26 +1,56 @@
 <?php
 
-    // cookie = Information about a user stored in a user's web-browser
-    //          targeted advertisements, browsing preferences, and
-    //          other non-sensitive data
+    // session = SGV used to store information on a user
+    //           to be used across multiple pages.
+    //           A user is assigned a session-id
+    //           ex. login credentials
 
-    setcookie("fav_food", "pizza", time() + (86400 * 2), "/");
-    setcookie("fav_drink", "coffee", time() + (86400 * 3), "/");
-    setcookie("fav_dessert", "ice cream", time() + (86400 * 4), "/");
+    session_start();
 
-    // setcookie("fav_food", "pizza", time() - 0, "/");
-    // setcookie("fav_drink", "coffee", time() - 0, "/");
-    // setcookie("fav_dessert", "ice cream", time() - 0, "/");
+?>
 
-    foreach($_COOKIE as $key => $value){
-        echo"{$key} = {$value} <br>";
-    }
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- This is the login page <br>
+    <a href="home.php">This goes to the home page</a> <br> -->
+    <form action="index.php" method="post">
+        username: <br>
+        <input type="text" name="username"> <br>
+        password: <br>
+        <input type="password" name="password"> <br>
+        <input type="submit" name="login" value="login">
+    </form>
 
-    if(isset($_COOKIE["fav_food"])){
-        echo"BUY SOME {$_COOKIE["fav_food"]} !!!";
-    }
-    else{
-        echo"I don't know your favorite food";
+</body>
+</html>
+
+<?php
+
+    if(isset($_POST["login"])){
+
+        
+
+        if(!empty($_POST["username"]) &&
+           !empty($_POST["password"])){
+
+            $_SESSION["username"] = $_POST["username"];
+            $_SESSION["password"] = $_POST["password"];
+
+            // echo $_SESSION["username"] . "<br>";
+            // echo $_SESSION["password"] . "<br>";
+
+
+            header("Location: home.php");
+        }
+        else{
+            echo"Missing username/password <br>";
+        }
     }
 
 ?>
