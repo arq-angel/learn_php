729b8da1cc004c5f695741dd9b6f69f614ddd42d
Arjun Thapa Magar <arjun2057@hotmail.com>
2023-08-02 18:31:12 +1000

    php PHP insert into MySQL database

---
+++ database.php
@@ -0,0 +1,27 @@
+<?php
+
+    $db_server = "localhost";
+    $bd_user = "root";
+    $db_pass = "";
+    $db_name = "businessdb";
+    $conn = "";
+
+
+    // Exception handling
+    try{
+        $conn = mysqli_connect($db_server, 
+                           $bd_user, 
+                           $db_pass, 
+                           $db_name);
+
+    }
+    catch(mysqli_sql_exception){
+        echo"Could not connect! <br>";
+    }
+
+    
+    if($conn){
+        echo"You are connected <br>";
+    }
+
+?>
