1d6996a95e9b97e6d624b5de3fed0c2f7ecbe6fa
Arjun Thapa Magar <arjun2057@hotmail.com>
2023-08-02 18:57:00 +1000

    php PHP registration form project

--- index.php
+++ index.php
@@ -1,19 +1,51 @@
 <?php
-
     include("database.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+        <h2>Welcome to Fakebook!</h2>
+        username: <br>
+        <input type="text" name="username"><br>
+        password: <br>
+        <input type="password" name="password"><br>
+        <input type="submit" name="submit" value="register">
+    </form>
+</body>
+</html>
+<?php
 
-    $sql = "SELECT * FROM users";
-    $result = mysqli_query($conn, $sql);
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+    
+        if(empty($username)){
+            echo"Please enter a username";
+        }
+        elseif(empty($password)){
+            echo"Please enter a password";
+        }
+        else{
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO users (user, password)
+                    VALUES ('$username', '$hash')";
 
-    if(mysqli_num_rows($result) > 0){
-        while($row = mysqli_fetch_assoc($result)){
-            echo $row["id"] . "<br>";
-            echo $row["user"] . "<br>";
-            echo $row["reg_date"] . "<br>";
-        };
-    }
-    else{
-        echo"No user found";
+            try{
+                mysqli_query($conn, $sql);
+            echo"You are now registered!";
+            }
+            catch(mysqli_sql_exception){
+                echo"That username is taken";
+            }
+            
+        }
+
     }
 
     mysqli_close($conn);
